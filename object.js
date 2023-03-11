6bc4dbd3778659db5d7455c42208b7769d84e4f8
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2023-03-12 02:35:53 +0300

    studied on Object of JS

---
+++ object.js
@@ -0,0 +1,59 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    id: 2345,
+    fullName: function () {
+        return 'first Name : '+this.firstName + ', last name :' + this.lastName; // this must be used in object
+    },
+    myFunction:  function () {
+        return this;
+    }
+}
+
+
+console.log("person.fullName  :\n\n " + person.fullName)
+console.log("person.fullName()  :\n\n " +person.fullName())
+
+console.log("person.myFunction  :\n\n " + person.myFunction) // return object function code  in String format
+console.log("person.myFunction()  :\n\n " +person.myFunction())// return object function result
+console.log("person.myFunction()  :\n\n " +person.myFunction().firstName) // return object's firstName parameter 
+
+const person1 = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const person2 = {
+    name:"Alice", // person1 has firstName but here it is name.
+    lastName: "Borderland",
+    age : 5
+  }
+  console.log(person1.fullName.call(person2));
+  
+
+
+// With the bind() method, an object can borrow a method from another object.
+const person3 = {
+  firstName:"Bob",
+  lastName: "Marlins",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+const member = {
+  firstName:"Hege",
+  lastName: "Nilsen",
+}
+
+let varTest = person3.fullName.bind(member); //if  variable fullName2 use like this return code String format. If it is used with bracktets then it works as function.
+console.log(varTest());
+
+/* 
+Precedence	Object
+1	bind()
+2	apply() and call()
+3	Object method
+4	Global scope
+*/
--- strictmode.js
+++ strictmode.js
@@ -1,14 +1,11 @@
 "use strict";
-console.log("STRICT MODE ENABLED : While this is enabled then variables without  declaring it cause errors");
+console.log("STRICT MODE ENABLED : While this is enabled then variables without  declaring it causes errors");
 let x= 10;
 console.log("Variable x is declared (let) : "+ x);
 
 var y= 20;
 console.log("Variable y is declared (var) : "+ y);
 
-console.log("Variable z is not declared : ");
-z = 30;  // This will cause an error (x is not defined).
-console.log("x :  "+x);
-
-
-    
+console.log("Variable z is not declared : (disabled bellow function right now)");
+// z = 30;  // This will cause an error (x is not defined).
+// console.log("z :  "+z);
